df494e3ecc3af8996f4e1c021c87e2d04d566183
say_no <dallas2112@yandex.ru>
2022-08-13 00:26:43 +0300

    feat: added grid for accordeons

--- pages/index.js
+++ pages/index.js
@@ -4,3 +4,13 @@ const popup = document.querySelector(".popup");
 menuButton.addEventListener("click", () => {
   popup.classList.toggle("popup_opened");
 });
+
+
+
+const accordion = document.querySelectorAll('accordion-body');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function(){
+    this.classList.toggle('active')
+  });
+}
