607f7fe1e4570d3dd94818cd988862305f63b2d9
Aleksandr Petrov <petrov.aleksr@gmail.com>
2022-08-12 21:48:36 +0500

    fix

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.querySelector(".header__menu-button");
-const popup = document.querySelector(".popup");
-
-menuButton.addEventListener("click", () => {
-  popup.classList.toggle("popup_opened");
-});
