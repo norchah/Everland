7f690faf2b09c18ffee192a703879bd7095bc1c8
say_no <dallas2112@yandex.ru>
2022-08-14 21:40:17 +0300

    fix: branch without js/gap between items

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,3 @@ menuButton.addEventListener("click", () => {
 });
 
 
-
-
-const accordion = document.querySelector('.accordion__items')
-accordion.addEventListener('click', toggleAccordion)
-const accordionHeadings = accordion.querySelectorAll('.accordion__heading');
-const accordionButton = accordion.querySelectorAll('.accordion__button');
-
-function toggleAccordion(evt) {
-  const accordionItem = evt.target.closest('.accordion__heading')
-  if (accordionItem) {
-      accordionItem.parentNode.classList.toggle('accordion__opened')
-      toggleOtherItems(accordionItem)
-  }
-}
-
-function toggleOtherItems(accordionTop) {
-  Array.from(accordionHeadings).forEach(top => {
-      if (top != accordionTop) {
-          top.parentNode.classList.remove('accordion__opened')
-      }
-  });
-}
