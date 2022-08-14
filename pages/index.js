ad19d03d119542a9f1bfb018be2e42d54000710c
Yulia Selivanova <selivanyulia@mail.ru>
2022-08-14 11:18:15 +0300

    feat: add smooth scrolling (donations to support)

--- pages/index.js
+++ pages/index.js
@@ -4,3 +4,20 @@ const popup = document.querySelector(".popup");
 menuButton.addEventListener("click", () => {
   popup.classList.toggle("popup_opened");
 });
+
+
+
+/*smooth scroll to support page*/
+const supportButton = document.querySelector('.button_support')
+
+supportButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const offsetTop = document.querySelector('#support').offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+});
+
+
