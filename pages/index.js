fccbc5797e2d13cb404ed34e474441be4c90bea7
Yulia Selivanova <selivanyulia@mail.ru>
2022-08-16 18:57:57 +0300

    feat: add function for linking radio-buttons from Donations and Support sections at submit

--- pages/index.js
+++ pages/index.js
@@ -21,3 +21,19 @@ supportButton.addEventListener('click', (e) => {
 });
 
 
+/*link radio-button with each other*/
+
+/*search for radio-buttons in DOM*/
+const radioButtonsDonations = document.querySelectorAll('input[name="donation"]');
+const radioButtonsSupport = document.querySelectorAll('input[name="donation-amount"]');
+
+/*link radio-buttons from Donations and Support sections at submit*/
+supportButton.addEventListener('click', function linkRadioButtons (e) {
+  e.preventDefault();
+  for (let i = 0; i < radioButtonsDonations.length; i++)
+  if (radioButtonsDonations[i].checked === true) {
+    radioButtonsSupport[i].checked = true;
+    console.log('ok!');
+  }
+});
+
