c254cd7897a0924e01ab12e1a0e4250e89633147
Yulia Selivanova <selivanyulia@mail.ru>
2022-08-17 00:00:15 +0300

    feat: add toggle input function

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ supportButton.addEventListener('click', (e) => {
 });
 
 
+
 /*link radio-button with each other*/
 
 /*search for radio-buttons in DOM*/
@@ -33,7 +34,55 @@ supportButton.addEventListener('click', function linkRadioButtons (e) {
   for (let i = 0; i < radioButtonsDonations.length; i++)
   if (radioButtonsDonations[i].checked === true) {
     radioButtonsSupport[i].checked = true;
-    console.log('ok!');
   }
 });
+
+
+
+/*show and hide donation amount field*/
+
+/*search for elements in DOM*/
+const inputsCollection = document.querySelectorAll('.form__item_text-input');
+const lastTextInput = inputsCollection[inputsCollection.length - 1];
+const lastRadioButtonDonations = radioButtonsDonations[radioButtonsDonations.length - 1];
+const lastRadioButtonSupport = radioButtonsSupport[radioButtonsSupport.length - 1];
+
+/*toggle donation amount field*/
+function toggleElement() {
+  if ((lastTextInput.style.display === "none" || lastTextInput.style.display === "")) {
+    lastTextInput.style.display = "block";
+  } else {
+    lastTextInput.style.display = "none";
+  }
+}
+
+/*hide donation amount field*/
+function hideElement() {
+//  if(!(lastRadioButtonDonations) || !(lastRadioButtonSupport))
+//  {
+    lastTextInput.style.display = "none";
+//  }
+}
+
+/*toggle donation amount field on click in both Donations and Support sections*/
+lastRadioButtonDonations.addEventListener('click', toggleElement);
+lastRadioButtonSupport.addEventListener('click', toggleElement);
+
+/*hide donation amount field on click*/
+//radioButtonsSupport.forEach(el => el.addEventListener('click', hideElement));
+radioButtonsSupport[0].addEventListener('click', hideElement);
+radioButtonsSupport[1].addEventListener('click', hideElement);
+radioButtonsSupport[2].addEventListener('click', hideElement);
+radioButtonsSupport[3].addEventListener('click', hideElement);
+radioButtonsDonations[0].addEventListener('click', hideElement);
+radioButtonsDonations[1].addEventListener('click', hideElement);
+radioButtonsDonations[2].addEventListener('click', hideElement);
+radioButtonsDonations[3].addEventListener('click', hideElement);
+
+
+
+
+
+
+
 
