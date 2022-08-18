59692fb3491cdf59fcc8c82f3028c02b2fb742f2
Aleksandr Petrov <petrov.aleksr@gmail.com>
2022-08-19 01:09:12 +0500

    ready!

--- pages/index.js
+++ pages/index.js
@@ -1,86 +1,24 @@
-const menuButton = document.querySelector(".header__menu-button");
-const popup = document.querySelector(".popup");
 
-menuButton.addEventListener("click", () => {
-  popup.classList.toggle("popup_opened");
+/* Переменные для секции Фотопроекты spec-projects*/
+let offset = 0; // cмещение от левого края
+const sliderLine = document.querySelector('.spec-projects__slider-line');
+const fotoSpecProjects = document.querySelector('.spec-projects__foto');
+const buttonLeftSpecProjects = document.querySelector('.spec-projects__button-left');
+const buttonRightSpecProjects = document.querySelector('.spec-projects__button-right');
+
+/* spec-projects Фотопроекты */
+buttonLeftSpecProjects.addEventListener('click', () => {
+  offset = offset + fotoSpecProjects.clientWidth;
+  if (offset > (sliderLine.clientWidth - fotoSpecProjects.clientWidth)) { offset = 0 }
+  sliderLine.style.left = -offset + 'px';
+  console.log(sliderLine.clientWidth);
 });
-
-
-
-/*smooth scroll to Support section*/
-const supportButton = document.querySelector('.button_location_donations-section')
-
-supportButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  const offsetTop = document.querySelector('#support').offsetTop;
-
-  scroll({
-    top: offsetTop - 80,
-    behavior: "smooth"
-  });
+buttonRightSpecProjects.addEventListener('click', () => {
+  offset = offset - fotoSpecProjects.clientWidth;
+  if (offset < 0) { offset = sliderLine.clientWidth - fotoSpecProjects.clientWidth }
+  sliderLine.style.left = -offset + 'px';
+  sliderLine.clientWidth;
 });
-
-
-
-/*link radio-button with each other*/
-
-/*search for radio-buttons in DOM*/
-const radioButtonsDonations = document.querySelectorAll('.form__item_donations-everland');
-const radioButtonsSupport = document.querySelectorAll('.form__item_donations-support');
-
-/*link radio-buttons from Donations and Support sections at submit*/
-supportButton.addEventListener('click', function linkRadioButtons (e) {
-  e.preventDefault();
-  for (let i = 0; i < radioButtonsDonations.length; i++)
-  if (radioButtonsDonations[i].checked === true) {
-    radioButtonsSupport[i].checked = true;
-  }
-});
-
-
-
-/*show and hide donation amount field*/
-
-/*search for elements in DOM*/
-const inputsCollection = document.querySelectorAll('.form__item_text-input');
-const lastTextInput = inputsCollection[inputsCollection.length - 1];
-const lastRadioButtonDonations = radioButtonsDonations[radioButtonsDonations.length - 1];
-const lastRadioButtonSupport = radioButtonsSupport[radioButtonsSupport.length - 1];
-
-/*toggle donation amount field*/
-function toggleElement() {
-  if ((lastTextInput.style.display === "none" || lastTextInput.style.display === "")) {
-    lastTextInput.style.display = "block";
-  } else {
-    lastTextInput.style.display = "none";
-  }
-}
-
-/*hide donation amount field in Support section*/
-function hideElement(event) {
-  if(event.target !== lastRadioButtonSupport) {
-    lastTextInput.style.display = "none";
-  }
-}
-
-/*show donation amount field from Donations section on submit*/
-function showElement() {
-  if (lastRadioButtonDonations.checked === true) {
-    lastTextInput.style.display = "block";
-  }
-}
-
-supportButton.addEventListener('click', showElement)
-
-/*toggle donation amount field on click in both Donations and Support sections*/
-lastRadioButtonDonations.addEventListener('click', toggleElement);
-lastRadioButtonSupport.addEventListener('click', toggleElement);
-
-/*hide donation amount field on click*/
-radioButtonsSupport.forEach((el) => el.addEventListener('click', hideElement));
-radioButtonsDonations.forEach((el) => el.addEventListener('click', hideElement));
-
-
 
 
 
