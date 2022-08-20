df23b12ffdb5a9bb2b8af2b174a911926b61c926
Aleksandr Petrov <petrov.aleksr@gmail.com>
2022-08-21 01:40:24 +0500

    мердж с develop, небольшие правки

---
+++ README.md
@@ -0,0 +1,26 @@
+# Командный проект
+
+### Секции:
+
+Я взял на себя смелость обозвать секции, если не нравится - можно смело переименовывать
+
+### Шрифты:
+
+Подключен шрифт, его значения:
+regular - 400, medium - 500, bold - 700
+
+## Тут сылки на макет и trello
+
+### Сслка на макет:
+
+**[Макет!](<https://www.figma.com/file/59a1PXM1KLWN0hWWMl1Kni/Everland-(Веб%2B)?node-id=430%3A713>)**
+
+## Ссылка на notion:
+
+**[Notion!](https://www.notion.so/Everland-3bcb8957004a49ec88d82c267d5757c0)**
+
+## Ссылка на trello:
+
+**[Trello!](https://trello.com/b/n4F7HbB5/workspace-team-6)**
+
+Приятной разработки!
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+const menuButton = document.querySelector(".header__menu-button");
+const popup = document.querySelector(".popup");
 
 /* Переменные и константы для слайдера*/
 let offset = 0; // значение cмещения от левого края
@@ -5,7 +7,87 @@ const windowOfSlider = document.querySelector('.slider-window');
 const buttonsRight = document.querySelectorAll('.slider-btn-right');
 const buttonsLeft = document.querySelectorAll('.slider-btn-left');
 
-/* Для слайдера */
+menuButton.addEventListener("click", () => {
+  popup.classList.toggle("popup_opened");
+});
+
+
+
+/*smooth scroll to Support section*/
+const supportButton = document.querySelector('.button_location_donations-section')
+
+supportButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const offsetTop = document.querySelector('.support').offsetTop;
+
+  scroll({
+    top: offsetTop - 80,
+    behavior: "smooth"
+  });
+});
+
+
+
+/*link radio-button with each other*/
+
+/*search for radio-buttons in DOM*/
+const radioButtonsDonations = document.querySelectorAll('.form__item_donations-everland');
+const radioButtonsSupport = document.querySelectorAll('.form__item_donations-support');
+
+/*link radio-buttons from Donations and Support sections at submit*/
+supportButton.addEventListener('click', function linkRadioButtons(e) {
+  e.preventDefault();
+  for (let i = 0; i < radioButtonsDonations.length; i++)
+    if (radioButtonsDonations[i].checked === true) {
+      radioButtonsSupport[i].checked = true;
+    }
+});
+
+
+
+/*show and hide donation amount field*/
+
+/*search for elements in DOM*/
+const inputsCollection = document.querySelectorAll('.form__item_text-input');
+const lastTextInput = inputsCollection[inputsCollection.length - 1];
+const lastRadioButtonDonations = radioButtonsDonations[radioButtonsDonations.length - 1];
+const lastRadioButtonSupport = radioButtonsSupport[radioButtonsSupport.length - 1];
+
+/*toggle donation amount field*/
+function toggleElement() {
+  if ((lastTextInput.style.display === "none" || lastTextInput.style.display === "")) {
+    lastTextInput.style.display = "block";
+  } else {
+    lastTextInput.style.display = "none";
+  }
+}
+
+/*hide donation amount field in Support section*/
+function hideElement(event) {
+  if (event.target !== lastRadioButtonSupport) {
+    lastTextInput.style.display = "none";
+  }
+}
+
+/*show donation amount field from Donations section on submit*/
+function showElement() {
+  if (lastRadioButtonDonations.checked === true) {
+    lastTextInput.style.display = "block";
+  }
+}
+
+supportButton.addEventListener('click', showElement)
+
+/*toggle donation amount field on click in both Donations and Support sections*/
+lastRadioButtonDonations.addEventListener('click', toggleElement);
+lastRadioButtonSupport.addEventListener('click', toggleElement);
+
+/*hide donation amount field on click*/
+radioButtonsSupport.forEach((el) => el.addEventListener('click', hideElement));
+radioButtonsDonations.forEach((el) => el.addEventListener('click', hideElement));
+
+
+/* Слайдер */
 const slideLeft = (event) => {
   const sliderWindow = event.currentTarget.closest('.slider-window');
   const sliderItems = sliderWindow.querySelectorAll('.slider-item');
@@ -45,22 +127,17 @@ const slideRight = (event) => {
   const btnLeft = sliderWindow.querySelector('.slider-btn-left');
   const btnRight = sliderWindow.querySelector('.slider-btn-right');
   const everlandCount = sliderWindow.querySelector('.everland__count');
-
   btnLeft.removeAttribute('disabled');
-
   let slidePosition = 0; // позиция элемента в слайдере
   let countItems = 0; // Количество элементов в слайдере
   let lineWidth = 0; // Общая длина всех элементов
   let itemWidth = 0; // Ширина элемента
-
   sliderItems.forEach(el => {
     countItems += 1;
   });
-
   lineWidth = countItems * sliderItems[0].clientWidth;
   itemWidth = sliderItems[0].clientWidth;
   offset = offset + itemWidth;
-
   if (offset > (lineWidth - itemWidth)) {
     offset = lineWidth - itemWidth;
   }
@@ -70,9 +147,7 @@ const slideRight = (event) => {
   if (offset > (lineWidth - itemWidth * 2)) {
     btnRight.setAttribute('disabled', 'disabled');
   }
-
   sliderLine.style.left = -offset + 'px';
-
   if (everlandCount) {
     slidePosition = countItems - ((lineWidth - offset) / itemWidth) + 1;
     everlandCount.innerHTML = slidePosition;
@@ -87,35 +162,12 @@ buttonsLeft.forEach(el => {
   });
   el.setAttribute('disabled', 'disabled');
 });
-
 /* "Вешаем прослушку" на правые кнопки слайдеров */
 buttonsRight.forEach(el => {
   el.addEventListener('click', (event) => {
     slideRight(event);
   });
 });
-
-/* const correctSlider = () => {
-  const sliderWindow = windowOfSlider.closest('.slider-window');
-  const sliderItems = windowOfSlider.querySelectorAll('.slider-item');
-  const sliderLine = windowOfSlider.querySelector('.slider-list');
-
-  let countItems = 0;
-  let lineWidth = 0;
-  let itemWidth = 0;
-  slidePosition = 0;
-
-  sliderItems.forEach(el => { countItems += 1; });
-
-  lineWidth = countItems * sliderItems[0].clientWidth;
-  itemWidth = sliderItems[0].clientWidth;
-
-
-  slidePosition = countItems - ((lineWidth - offset) / itemWidth) + 1;
-}; */
-
-/* correctSlider(); */
-/* window.addEventListener('resize', correctSlider); */
 
 
 
