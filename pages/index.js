d9038daa7c3275cc7d04d5108c8e347c1b0c8ce7
Aleksandr Petrov <petrov.aleksr@gmail.com>
2022-08-20 17:34:43 +0500

    Реализована основная функциональность JS для слайдеров

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,121 @@
 
-/* Переменные для секции Фотопроекты spec-projects*/
-let offset = 0; // cмещение от левого края
-const sliderLine = document.querySelector('.spec-projects__slider-line');
-const fotoSpecProjects = document.querySelector('.spec-projects__foto');
-const buttonLeftSpecProjects = document.querySelector('.spec-projects__button-left');
-const buttonRightSpecProjects = document.querySelector('.spec-projects__button-right');
+/* Переменные и константы для слайдера*/
+let offset = 0; // значение cмещения от левого края
+let slidePosition; // позиция элемента в слайдере
+let countItems; // Количество элементов в слайдере
+let lineWidth; // Общая длина всех элементов
+let itemWidth; // Ширина элемента
+const windowOfSlider = document.querySelector('.slider-window');
+const buttonsRight = document.querySelectorAll('.slider-btn-right');
+const buttonsLeft = document.querySelectorAll('.slider-btn-left');
+
+
 
 /* Для слайдера */
-const slidePictureLeft = (line, foto, btnLeft, btnRight) => {
+const slideLeft = (event) => {
+  const sliderWindow = event.currentTarget.closest('.slider-window');
+  const sliderItems = sliderWindow.querySelectorAll('.slider-item');
+  const sliderLine = sliderWindow.querySelector('.slider-list');
+  const btnLeft = sliderWindow.querySelector('.slider-btn-left');
+  const btnRight = sliderWindow.querySelector('.slider-btn-right');
+
+  let countItems = 0;
+  let lineWidth = 0;
+  let itemWidth = 0;
+  slidePosition = 0;
+
   btnRight.removeAttribute('disabled');
-  offset = offset - foto.clientWidth;
+  sliderItems.forEach(el => { countItems += 1; });
+  lineWidth = countItems * sliderItems[0].clientWidth;
+  itemWidth = sliderItems[0].clientWidth;
+  offset = offset - itemWidth;
   if (offset < 0) {
     offset = 0
   }
-  if (offset < foto.clientWidth) {
+  sliderLine.style.left = -offset + 'px';
+  if (offset < itemWidth) {
     btnLeft.setAttribute('disabled', 'disabled');
   }
-  line.style.left = -offset + 'px';
-  line.clientWidth;
+
+  slidePosition = countItems - ((lineWidth - offset) / itemWidth) + 1;
+  console.log(slidePosition);
 };
 
-const slidePictureRight = (line, foto, btnLeft, btnRight) => {
+const slideRight = (event) => {
+  const sliderWindow = event.currentTarget.closest('.slider-window');
+  const sliderItems = sliderWindow.querySelectorAll('.slider-item');
+  const sliderLine = sliderWindow.querySelector('.slider-list');
+  const btnLeft = sliderWindow.querySelector('.slider-btn-left');
+  const btnRight = sliderWindow.querySelector('.slider-btn-right');
+
   btnLeft.removeAttribute('disabled');
-  offset = offset + foto.clientWidth;
-  if (offset > (line.clientWidth - foto.clientWidth)) {
-    offset = line.clientWidth - foto.clientWidth;
+
+  let countItems = 0;
+  let lineWidth = 0;
+  let itemWidth = 0;
+  slidePosition = 0;
+
+  sliderItems.forEach(el => {
+    countItems += 1;
+  });
+
+  lineWidth = countItems * sliderItems[0].clientWidth;
+  itemWidth = sliderItems[0].clientWidth;
+  offset = offset + itemWidth;
+
+  if (offset > (lineWidth - itemWidth)) {
+    offset = lineWidth - itemWidth;
   }
-  line.style.left = -offset + 'px';
-  if (offset > (line.clientWidth - (2 * foto.clientWidth))) {
+  if (offset > lineWidth) {
+    offset = offset - itemWidth;
+  }
+  if (offset > (lineWidth - itemWidth * 2)) {
     btnRight.setAttribute('disabled', 'disabled');
   }
+
+  sliderLine.style.left = -offset + 'px';
+
+  slidePosition = countItems - ((lineWidth - offset) / itemWidth) + 1;
+  console.log(slidePosition);
 }
 
-buttonRightSpecProjects.addEventListener('click', () => {
-  slidePictureRight(sliderLine, fotoSpecProjects, buttonLeftSpecProjects, buttonRightSpecProjects);
+/* "Вешаем прослушку" на левые кнопки слайдеров
+и отключаем все левые кнопки */
+buttonsLeft.forEach(el => {
+  el.addEventListener('click', (event) => {
+    slideLeft(event);
+  });
+  el.setAttribute('disabled', 'disabled');
 });
 
-buttonLeftSpecProjects.addEventListener('click', () => {
-  slidePictureLeft(sliderLine, fotoSpecProjects, buttonLeftSpecProjects, buttonRightSpecProjects);
+/* "Вешаем прослушку" на правые кнопки слайдеров */
+buttonsRight.forEach(el => {
+  el.addEventListener('click', (event) => {
+    slideRight(event);
+  });
 });
 
-buttonLeftSpecProjects.setAttribute('disabled', 'disabled');
+/* const correctSlider = () => {
+  const sliderWindow = windowOfSlider.closest('.slider-window');
+  const sliderItems = windowOfSlider.querySelectorAll('.slider-item');
+  const sliderLine = windowOfSlider.querySelector('.slider-list');
+
+  let countItems = 0;
+  let lineWidth = 0;
+  let itemWidth = 0;
+  slidePosition = 0;
+
+  sliderItems.forEach(el => { countItems += 1; });
+
+  lineWidth = countItems * sliderItems[0].clientWidth;
+  itemWidth = sliderItems[0].clientWidth;
+
+
+  slidePosition = countItems - ((lineWidth - offset) / itemWidth) + 1;
+}; */
+
+/* correctSlider(); */
+/* window.addEventListener('resize', correctSlider); */
 
 
 
